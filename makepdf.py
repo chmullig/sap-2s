2e50d7970e6ceafb1f0a6e6f72183651eafa5419
Chris Mulligan <chris.mulligan@gmail.com>
2017-09-16 21:14:13 -0400

    Updated documentation. README instead of intro.

--- makepdf.py
+++ makepdf.py
@@ -2,26 +2,32 @@ import sys, os
 import csv
 import tempfile
 import subprocess
+import shutil
 
-SELECT_CMD = "gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -dFirstPage={start} -dLastPage={end} -sOutputFile=\"{destination}\" \"{file}\""
+SELECT_CMD = "gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -dFirstPage={start} -dLastPage={end} -sOutputFile=\"{destination}\" \"{source}\""
 MERGE_CMD = " gs  -o {merged}  -sDEVICE=pdfwrite  -dColorConversionStrategy=/LeaveColorUnchanged  -dEncodeColorImages=false  -dEncodeGrayImages=false  -dEncodeMonoImages=false {files}"
 
 tmpdir = tempfile.mkdtemp()
 print(tmpdir)
 files = []
-subprocess.run("pandoc intro.md -s -o intro.pdf", shell=True)
+subprocess.run("pandoc README.md -s -o README.pdf", shell=True)
 with open("contents.csv") as csvf:
     contents = csv.DictReader(csvf)
     for i, entry in enumerate(contents):
         if entry['start']:
             destination = "{}_{}".format(i, entry['file'])
-            files.append("\""+tmpdir+"/"+destination+"\"")
-            cmd = SELECT_CMD.format(destination=tmpdir+"/"+destination, **entry)
+            fn = os.path.join(tmpdir, entry['directory'] + destination)
+            files.append("\""+fn+"\"")
+            cmd = SELECT_CMD.format(destination=fn,  source=os.path.join(entry['directory'], entry['file']), **entry)
             print(cmd)
             subprocess.run(cmd, shell=True)
         else:
-            files.append("\""+entry['file']+"\"")
+            fn = os.path.join(entry['directory'], entry["file"])
+            files.append("\""+fn+"\"")
 print(tmpdir)
+print(files)
 cmd = MERGE_CMD.format(merged="merged.pdf", files=" ".join(files))
 print(cmd)
 subprocess.run(cmd, shell=True)
+
+shutil.rmtree(tmpdir)
